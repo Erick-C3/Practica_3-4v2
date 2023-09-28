a892b3991c6a6110849e12e12058d886bd91c6ae
Erick <erick.nahuelpaiva.001@gmail.com>
2023-09-28 12:18:57 -0300

    practica 3-4

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const Enemigo = require("./Enemigo.js");
-const PowerRanger = require("./PowerRanger.js");
-const SEPARACION = '\x1b[36m%s\x1b[0m';
+const ProgramaTv = require("./ProgramaTv.js");
+const enemigos = require("./enemigos.js");
+const powerRangers = require("./powerRangers.js");
 
-/* const unPowerRanger = new PowerRanger("Red", 5, 12);
-const enemigo1 = new Enemigo("Zoor", 2, 4);
-const otroPowerRanger = new PowerRanger("Green", 3.4, 10);
-const enemigo2 = new Enemigo("Rita", 7, 12);
-const unoMasPowerRanger = new PowerRanger("Black", 7, 20); */
 
-const powerRangers = [
-    new PowerRanger("Red", 5, 12),
-    new PowerRanger("Green", 3.4, 10),
-    new PowerRanger("Black", 7, 20)
-];
+const programaPowerRangers = new ProgramaTv(enemigos, powerRangers);
 
-const enemigos = [
-    new Enemigo("Zoor", 2, 4),
-    new Enemigo("Rita", 7, 12)
-];
+programaPowerRangers.iniciaEpisodio();
 
-powerRangers[2].atacaA(enemigos[0]);
+/* powerRangers[2].atacaA(enemigos[0]);
 console.log(SEPARACION, "--------------");
 powerRangers[0].atacaA(enemigos[0]);
 console.log(SEPARACION, "--------------");
 enemigos[1].atacaA(powerRangers[1]);
+enemigos[1].atacaA(powerRangers[2]);
+console.log(SEPARACION, "--------------");
+powerRangers[2].atacaA(enemigos[1]);
+console.log(SEPARACION, "--------------");
+enemigos[0].atacaA(powerRangers[2]);
+ */
 
 
 /* 
---
+++ powerRangers.js
@@ -0,0 +1,11 @@
+/* const PowerRanger = require("./PowerRanger.js"); */
+
+const PowerRanger = require("./PowerRanger.js");
+
+const powerRangers = [
+    new PowerRanger("Red", 5, 12),
+    new PowerRanger("Green", 3.4, 10),
+    new PowerRanger("Black", 7, 20)
+];
+
+module.exports = powerRangers;
